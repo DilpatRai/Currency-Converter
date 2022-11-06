916478e896ce55b9a85bd8fe8e226859ddc2501b
DilpatRai <dilpatrai34@gmail.com>
2022-11-07 00:25:42 +0500

    MAD Project

---
+++ README.md
@@ -0,0 +1,6 @@
+# live_currency_converter
+
+<center><img src="preview.png" width="800px"></center>
+
+### API used: [exchangerate-api](https://www.exchangerate-api.com)
+#### To build this app, please acquire an `API key` from above site and put into `lib/constants.dart`
---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundColor = Color(0xFF181823);
+const kLightColor = Color(0xFF212130);
+const kButtonColor = Color(0xFFC5DECE);
+const kGreyishColor = Color(0xFFf4f7fa);
+
+const kTitleSize = 28.0;
+const kSubtitleSize = 18.0;
+
+const kDefaultPadding = 18.0;
+
+const API_KEY = '2f9c212032fd0364a7e8849b';
+const BASE_URL = 'v6.exchangerate-api.com/v6';
+const CURRENCIES_CODES_ENDPOINT = 'codes';
+const RATES_ENDPOINT = 'latest';
